57d65f90507310b65153947814f490b94ee52d93
Intouch Marsvongpragorn <intouch.mars@gmail.com>
2017-05-30 12:56:04 +0700

    Added FeedDetailViewController UX and swift file
    - Added FeedDetailViewController
    - Added UI of FeedDetailViewController

---
+++ MyFirstProject/MyFirstProject/FeedDetailViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FeedDetailViewController.swift
+//  MyFirstProject
+//
+//  Created by IndyZa on 5/30/2560 BE.
+//  Copyright © 2560 IndyZaLab. All rights reserved.
+//
+
+import UIKit
+
+class FeedDetailViewController: UIViewController {
+
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var accessoryLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var headerImgView: UIImageView!
+    
+    var news:News?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.news = News(title: "iOS", description: "Error lets stop doing this", iconImage: #imageLiteral(resourceName: "cheese_flat"), author: "พยุต", view: 2, createDate: "29/06/60")
+        // Do any additional setup after loading the view.
+        setupUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClickSaveButton(_ sender: Any) {
+        print("Click Save Button")
+        
+    }
+
+    
+    func setupUI(){
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
