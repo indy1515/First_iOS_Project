136a17e2ed423c662a09640e720ac502c3497591
Intouch Marsvongpragorn <intouch.mars@gmail.com>
2017-06-01 10:40:59 +0700

    Added Mapping in News Object

--- MyFirstProject/MyFirstProject/News.swift
+++ MyFirstProject/MyFirstProject/News.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import ObjectMapper
 
-class News {
-    var id:Int
-    var title:String
-    var description:String
-    var iconImage:UIImage
-    var author:String
-    var view:Int
-    var createDate:String
-    var category:String
+class News:Mappable {
+    var id:Int = -1
+    var title:String = ""
+    var description:String = ""
+    var iconImage:UIImage = UIImage()
+    var imageUrl:String = ""
+    var author:String = ""
+    var view:Int = -1
+    var createDate:String = ""
+    var updateDate:String = ""
+    var category:String = ""
     
     init(id:Int,title:String, description:String, iconImage:UIImage, author:String, view:Int, createDate:String, category:String = "Undefined") {
         self.id = id
@@ -27,6 +30,22 @@ class News {
         self.view = view
         self.createDate = createDate
         self.category = category
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        self.id <- map["id"]
+        self.title <- map["title"]
+        self.description <- map["description"]
+        self.imageUrl <- map["image_url"]
+        self.author <- map["author"]
+        self.view <- map["total_view"]
+        self.createDate <- map["created_at"]
+        self.updateDate <- map["updated_at"]
+        self.category <- map["category"]
     }
 
     
